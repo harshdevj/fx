68e1a6cd04f176441b06b247f4b439b58ff2b7b7
harshdevj <harsh.joshi22@gmail.com>
2017-04-25 00:15:33 -0400

    test

--- src/app/transactionHistory/transaction-history.component.ts
+++ src/app/transactionHistory/transaction-history.component.ts
@@ -6,11 +6,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionHistoryComponent implements OnInit {
 
+    private transList = [
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' },
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' },
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' },
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' },
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' },
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' },
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' },
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' },
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' },
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' },
+        { trNo: 1001, payAcc: 11233, sentAmt: 123456, rate: 65.5, recAccNum: 2321315464, recAmt: 564456455, status: 'Approved', createdOn: '1-Jan-2016', approveRejected: '1-Jan-2016' }
+    ];
+
     constructor() {
     }
 
     ngOnInit() {
-        
+
     }
 
 }
